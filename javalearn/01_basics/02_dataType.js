f8679ce7d87199bccbc6ad4f6e6f1b3ec8a051c5
LazyCoder <78717983+Deepalikashyap9901@users.noreply.github.com>
2024-07-23 14:18:36 +0000

    datatype

---
+++ javalearn/01_basics/02_dataType.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version //
+
+// alert( 3 + 3) // we are using node , not browser
+
+console.log( 3
+    +
+    3) // code readebility should be high
+
+    console.log("Deepali");
+
+    let Name = "Deepali"
+    let age = 18
+    let isLoggedIn = false
+
+    // number => 2 to power 53
+    // bigint
+    // string => "
+    // boolean => true/false
+    // null => standlone value
+    // undefined => 
+    // symbol => unique
+    //object
+
+    console.log(typeof isLoggedIn);
+    console.log(typeof Object);
+   // console.log(typeof null);// null output => object
+    
+   // console.log(typeof undefined); // undefined output => undefined 
