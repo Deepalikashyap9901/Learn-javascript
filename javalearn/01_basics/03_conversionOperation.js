77fab477c690873c4e4717afea53c3cd75bb5570
LazyCoder <78717983+Deepalikashyap9901@users.noreply.github.com>
2024-07-23 18:21:31 +0000

    conversion of dataType

---
+++ javalearn/01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score = false
+
+// console.log(typeof score);
+console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33 but
+// "33abc" => NaN 
+// NaN mean => not a number
+// true => 1 , false => 0
+
+// let isLoggedIn = "Deepali"
+
+// let booleanLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanLoggedIn);
+
+// 1 => true , 0 => false , null => False 
+// "Deepali" => true
+
+let someNumber = undefined
+
+let StringNumber = Number(someNumber)
+console.log(StringNumber);
+console.log(typeof(StringNumber));
+
+// 11 => 11
+// true => 1 , false => 0 , Name => NaN
+// null => 0 , object => NaN 
+// undefined => NaN
+
+// ************************ operations ************************* //
+
