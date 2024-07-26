cf32077577e364df8af65c4837da099622cdccef
LazyCoder <78717983+Deepalikashyap9901@users.noreply.github.com>
2024-07-26 10:17:27 +0000

    03_conversionOperation

--- javalearn/01_basics/03_conversionOperation.js
+++ javalearn/01_basics/03_conversionOperation.js
@@ -1,7 +1,7 @@
 let score = false
 
 // console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof(score));
 
 // let valueInNumber = Number(score)
 
@@ -21,16 +21,45 @@ console.log(typeof(score));
 // 1 => true , 0 => false , null => False 
 // "Deepali" => true
 
-let someNumber = undefined
-
-let StringNumber = Number(someNumber)
-console.log(StringNumber);
-console.log(typeof(StringNumber));
 
 // 11 => 11
 // true => 1 , false => 0 , Name => NaN
 // null => 0 , object => NaN 
 // undefined => NaN
 
-// ************************ operations ************************* //
 
+/* let value = 22
+let negValue = -value
+
+console.log(negValue); */
+
+// console.log(2+2);
+///console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%2);
+
+//let str1 = "Hello"
+//let str2 = " Abhi"
+//let str3 = str1 + str2
+//console.log(str3);
+
+/*console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");*/
+
+/*console.log((3+5) * 3 % 8 );
+
+console.log(true);
+console.log(+true);*/
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+let gameCounter2 = 100
+++gameCounter2
+console.log(gameCounter);
