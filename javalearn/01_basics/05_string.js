352bda733089707457e5a257d367887b090e53be
LazyCoder <78717983+Deepalikashyap9901@users.noreply.github.com>
2024-07-27 08:44:48 +0000

    String

---
+++ javalearn/01_basics/05_string.js
@@ -0,0 +1,40 @@
+const Name = "Deepali"
+const score = 50
+
+// console.log(Name + score + "value")
+
+//console.log(`Hello my name is ${Name} and my score is ${score}`);
+
+
+const gameName = new String('DeepaliKK')
+
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.toLowerCase());
+//console.log(gameName.charAt(3));
+//console.log(gameName.indexOf('l'));
+
+
+//const newString = gameName.substring(0, 4);
+//console.log(newString);
+
+//const otherString = gameName.slice(-7, 4)
+//console.log(otherString);
+
+/* const newStringOne = "    Deepali       "
+console.log(newStringOne.trim());
+console.log(newStringOne.trimStart());
+console.log(newStringOne.trimEnd()); */
+
+
+const url = "https://Deepali.com/deepali%20kashyap"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('Riya'));
+console.log(url.includes('Deepali'));
+
+console.log(gameName.split('-'));
